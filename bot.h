40c54d1eb69011c3c27626f1066fece561bbc452
Egor Lebedev <eslebedev552002@gmail.com>
2021-12-21 23:28:21 +0300

    добавлен bot.h с объявлением функций и заготовки для включения других файлов

--- bot.h
+++ bot.h
@@ -1,4 +1,6 @@
+#include
+#include
 bool chooseMove(Snake* bot, std::string level, Snake enemy, Apple apple);
-bool hardMode(Snake* bot, Snake enemy, Apple apple); //chooseBestMove
-bool normalMode(Snake* bot, Snake enemy, Apple apple); //chooseCommonMove
-bool goToApple(Snake* bot, Snake enemy, Apple apple); //wantApple
+bool hardMode(Snake* bot, Snake enemy, Apple apple);
+bool normalMode(Snake* bot, Snake enemy, Apple apple); 
+bool goToApple(Snake* bot, Snake enemy, Apple apple); 
