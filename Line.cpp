6fbc97cacae9b00ed612a6c8f05ad94cfccebc3c
Egor Lebedev <eslebedev552002@gmail.com>
2021-12-22 21:46:28 +0300

    добавлены функции изменения координат и возвращения координат

--- Line.cpp
+++ Line.cpp
@@ -9,4 +9,34 @@ Line::Line(int x, int y)
 }
 
 
+//‘ункци€ измен€юща€ координаты
+void Line::changePos(int x, int y)
+{
+	beforeX = nowX;
+	beforeY = nowY;
+	nowX = x;
+	nowY = y;
+}
 
+
+//‘ункции возвращающие значени€ координат
+int Line::getNowX()
+{
+	return nowX;
+}
+
+int Line::getNowY()
+{
+	return nowY;
+}
+
+int Line::getBeforeX()
+{
+	return beforeX;
+}
+
+
+int Line::getBeforeY()
+{
+	return beforeY;
+}
